54c5014905dfc2e5ce3cac7442a7133090e7e953
Ria Pacheco <ria.pacheco@coldboretechnology.com>
2022-09-18 00:38:47 -0600

    Declarations errors resolved by skipping strict typescript checking for tensforflow lib specifically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'img-ai';
+export class AppComponent implements OnInit {
+  model!: any;
+  loading!: boolean;
+
+
+  async ngOnInit() {
+    this.loading = true;
+    this.model = await mobilenet.load();
+    this.loading = false;
+  }
 }
