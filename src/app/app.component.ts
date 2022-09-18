0a889300e9d7824bb60dea84b2e439898b3456f2
Ria Pacheco <ria.pacheco@coldboretechnology.com>
2022-09-18 01:01:15 -0600

    double check if module missing later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mobilenet from '@tensorflow-models/mobilenet';
+
+export interface IPrediction {
+  className: string;
+  probability: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,13 +12,55 @@ import * as mobilenet from '@tensorflow-models/mobilenet';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  imageSrc!: string;
+  @ViewChild('img') imageEl!: ElementRef;
+
+  predictions!: IPrediction[];
+  
   model!: any;
   loading!: boolean;
+
+  imgSrc!: any;
 
 
   async ngOnInit() {
     this.loading = true;
     this.model = await mobilenet.load();
     this.loading = false;
+  }
+
+  async fileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = (res: any) => {
+        this.imgSrc = res.target.result;
+
+        setTimeout(async () => {
+          this.predictions = await this.model.classify(this.imageEl.nativeElement);
+        })
+      }
+    }
+  }
+
+  async fileChangeEvent(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (res:any) => {
+        this.imageSrc = res.target.results;
+
+        setTimeout(async () => {
+          const imgEl = res.target.result;
+          this.predictions = await this.model.classify(imgEl);
+        }, 0);
+      }
+    }
   }
 }
